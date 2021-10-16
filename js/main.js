99f8f814659fd9b286ae531a6ad6945cc1a024bf
MohMostafa <m.mostafa2112@gmail.com>
2021-10-16 14:20:27 +0200

    updating new feature

--- js/main.js
+++ js/main.js
@@ -5,14 +5,16 @@ const navList = document.querySelector("nav ul");
 const navLinks = document.querySelectorAll("nav li a");
 const overlay = document.querySelector(".overlay");
 const footerLinks = document.querySelectorAll("footer li a");
+const scrollTopBtn = document.querySelector(".scroll-top-btn");
 
-nav.onclick = function (e) {
-  if (e.target.classList.contains("open-menu") || e.target.classList.contains("close-menu")) {
-    openMenu.classList.toggle("active");
-    closeMenu.classList.toggle("active");
-    navList.classList.toggle("visible");
-    overlay.classList.toggle("visible");
-    document.body.classList.toggle("scroll-locked");
+// toggle Mobile Navbar
+document.body.onclick = function (e) {
+  if (e.target.classList.contains("open-menu")) {
+    
+    showMobileNav();
+  } else {
+    
+    hideMobileNav();
   }
 };
 
@@ -24,6 +26,9 @@ function scrollToSection(links) {
       if(this.dataset.section) {
         document.querySelector(this.dataset.section).scrollIntoView({behavior: "smooth", block: "start"});
       }
+      if (navList.classList.contains("visible")) {
+        hideMobileNav();
+      }
     }
   });
 }
@@ -31,3 +36,47 @@ function scrollToSection(links) {
 // When click on a sepcific link, scroll to a sepcific section
 scrollToSection(navLinks);
 scrollToSection(footerLinks);
+
+
+// When window is resized, hide Mobile Navbar
+window.onresize = hideMobileNav;
+
+
+// When you click on Scroll To Top Button, Go to the top of the page
+scrollTopBtn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+
+// toggle Scroll To Top Button
+window.onscroll = function () {
+  console.log(window.scrollY);
+  if (window.scrollY >= 400) {
+    scrollTopBtn.classList.add("visible");
+  } else {
+    scrollTopBtn.classList.remove("visible");
+  }
+}
+
+/** Helper Functions */
+
+// Create Function to show Mobile Navbar
+function showMobileNav() {
+  openMenu.classList.remove("active");
+  closeMenu.classList.add("active");
+  navList.classList.add("visible");
+  overlay.classList.add("visible");
+  document.body.classList.add("scroll-locked");
+}
+
+// Create Function to hide Mobile Navbar
+function hideMobileNav() {
+  openMenu.classList.add("active");
+  closeMenu.classList.remove("active");
+  navList.classList.remove("visible");
+  overlay.classList.remove("visible");
+  document.body.classList.remove("scroll-locked");
+}
+
